b477fef3a04e8098a7df8a7da56a987b8c754162
Cameron Y <cameronslatton@gmail.com>
2020-08-11 23:14:20 -0500

    Project update with base classes

---
+++ SimpleStoreAPI/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SimpleStoreAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// This is to stop a circular reference
+        /// </summary>
+        [JsonIgnore]
+        public virtual User User { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+}
---
+++ SimpleStoreAPI/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SimpleStoreAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// This is to stop a circular reference
+        /// </summary>
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+    }
+}
