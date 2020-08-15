754f50bfd664d0ef72139d83227e8cd1bc1f5aa0
Cameron Y <cameronslatton@gmail.com>
2020-08-15 14:58:54 -0500

    updated launchsettings

--- SimpleStoreAPI/Controllers/ProductsController.cs
+++ SimpleStoreAPI/Controllers/ProductsController.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SimpleStoreAPI.Models;
 
 namespace SimpleStoreAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(_context.Products.ToArray());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            return Ok(product);
+        }
     }
 }
